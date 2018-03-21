42d4fb1d0938b6c98b4dd4568d9f2cfa6c27cb09
andrienko <evolmate@gmail.com>
2018-03-21 11:43:01 +0200

    First release version

--- README.md
+++ README.md
@@ -1,179 +1,51 @@
 Taj
 ===
-Taj is a yet another piping mechanism. I needed something like this one so I wrote one.
-Probably in future I will develop it somehow.
+Taj Asynchronous Jammer is a yet another piping mechanism for JS.
 
-To be honest it is a rarther weird mechanism, but it is made to suit perfectly some of my needs.
+Here's an example of use:
 
-The story is I wrote it and then decided it's reuseable, so I started picking a cool name for it and it turned out all
-the cool suitable names were already taken by dozens of libs doing similar things (yeah, I'm reinventing the wheel
-basically, but it's a good thing this time). I'm writing the library for personal use basically, but any help/questions
-are welcome.
-
-"taj" stands for "taj asynchronous jammer"
-
-How does it work?
----
-It just lets you define a set of methods and then chain them, passing arguments if necessary and passing a (single)
-variable through the pipe if needed.
-
-That is - first you define methods, and only then run all or some of them. By chaining you are not running anything -
-instead you are adding methods to queue, that will be launched only when you call a `go()` method. 
-
-My usual problem is organizing async code. I believe there's no need in promises and deferred calls - because one could
-simply define a set of methods inside some kind of objects and call them from inside of each other in whatever order
-one needs. 
- 
-### Usage
-
-You have to create an instance and add methods to it (you can pass them in constructor). Then (if you need it) you set 
-the initial value and chain methods you've just defined. Then you add the last, `.go` method, which receives a function
-that will be called once the chaining is done. The function will receive the resulting value (if there were any).
-
-    var taj = require('taj');
+    var Taj = require('./taj');
+    var chain = new Taj();
     
-    var chain = new taj();
-
-    // Define the method called "upper"
-
-    // Methods receive two parameters - the current value
-    // and the callback that should be called once everything
-    // is done (instead of return)
-
-    chain.link('upper', function (input, output){
-         output(input.toUpperCase())
-    });
-
-
-    // In addition, they will receive all the arguments
-    // you pass while chaining:
-
-    chain.link('add', function (input, output, string) {
-        output(input + string);
+    // Creating new callback
+    chain.registerCallback('add',function(pass, value){
+      pass(this+value); // this will be the walue ".run" function below is called with.
     });
     
-    // Now you can chain methods you've defined:
-
-    chain.set('hello')               // setting the initial value
-
-      .add(', world!')                 
-      .upper()
-      .add('.. Hey!')
-
-      .go(function (output) {        // command to start chain
-          console.log(output);       // that receives a callback
-        }                            // that will be called once
-      )                              // chaining is done
-    ;
-
-You can shorten things up by passing initial value and chain links inside the constructor:
-
-    var chain = new taj('hello',{
-      upper:function (input, output){
-        output(input.toUpperCase())
-      },
-      add:function (input, output, string) {
-        output(input + string);
-      }
-
-    });
-
-That is pretty much all
-
-### Methods
-
-Some method names are reserved and you can not use them as names for your links (well you can, but it may break
-something)
-
-#### link
-
-Creates a "link" in your "chain". Creating a link does not run your code - instead it creates the method of your
-chaining object. So, after typing
-
-    var chain = new taj([]);
-    chain.link('hero', function(heroes,output,name){
-      heroes.push(name);
-      output(heroes);
-    });
-
-You create a new method you can call:
-
-    chain.hero('batman').hero('iron man').go(function(heroes){
-      console.log('Superheroes: ' + heroes.join(', '));
-    });
-
-This should output "Superheroes: batman,iron man"
-
-#### next (callback)
-
-Adds an anonymous link to the execution pipe
-
-#### onError (callback)
-
-Defines an error logger. This is not really meant for exceptions catching tho. Only to not depend on console.
-
-the lib silently ignores errors in your links by default. You can use the onError method to set error handler
-
-    new taj()
-        .link('test',function(){meh();})
-        .onError(function(error_obj){console.log(error_obj.text, error_obj.exception);})
-        .test()
-        .go();
-
-Should output something like `Error in test [ReferenceError: meh is not defined]`
-
-#### onReady (callback)
-
-Defines a callback that will be executed when chaining methods are finished.
-Will receive a current value if it is defined/used
-
-#### set (name, callback)
-
-Sets the value of the output. You could have written this method yourself: 
-
-    taj.link('set', function(input,output,value){ output(value) });
-
-#### go([callback], [value])
-
-Nothing will be executed until this method is called. 
-
-- If callback is passed - it will be same as calling `onReady(callback).go()`
-- If value is passed - it will be same as calling `set(value).onReady(callback).go()`
-
-### So, what about asynchronous calls?
-
-Here's an idea then. Let's imagine we're in browser:
-
-    var AJAX = new taj('', {
-      getBody:function (input, output, url) {
-        var request = new XMLHttpRequest();
-        request.open('GET', url, true);
+    // now "add" function is registered and can be chained
     
-        request.onload = function () {
-          var result = '';
-          if(this.status>=200 && this.status<400){
-            try{
-              result = JSON.parse(this.response);
-            } catch (e) {}
-          }
-          output(result);
-        }
-
-      }
-    });
-
-Now, calling something like this:
+    // Adding it to chain's queue. Twice
+    chain
+      .add(', world')
+      .add('!');
     
-    AJAX.getBody('http://your.url').go(function (data) {
-      console.log(data);
+    // Running the chain
+    chain.run('Hello',function(){
+      console.log(this);
     });
+    
+    // Output will be 'Hello, world!' as a String
+    // (primitive gets transformed to obj after passed through apply)
+    
+The idea is you register several functions and define a queue of them and their arguments. 
 
-..will result in parsed JSON (or an empty string on error) to be outputted to console.
+You can register multiple functions at once using `.registerCallbacks` method, passing it an object which keys will be
+funciton names and values should be callbacks.
 
-Yeah, you could've done that with jQuery's chaining mechanism. Or promises. Or whatever.
+You may use .then to add a function to the queue without registering it
 
-### Caveats
+    chain
+      .then(function(pass){
+          pass(this+'test');
+      })
+      
+ - You can also pass parameters array as a second argument of `.then`, these will be applied just like with registered
+functions.
+ - If you need a different queue - normally you should create a different chain (different Taj object), but also you can
+ use the `.resetQueue` method, that will clear the queue.
+ - getFunction method will return a function that does the same as if you wrote chain.run
+ - You can access queue and callbacks via chain.__state value 
+ - When passing primitive values through the chain - the object will be returned (because `apply` is used). If you want
+ to explicitly use primitive values the best way would be wrapping them with an object, something like 
+ `.run({value: 'Hello'}, function(){ ... })`
 
- - The "output" function is also passed as `this` argument to all the links. Also it has two properties - `chain`, which contains a link to current chain object, and `error` which is a function triggering `onError` callback.
- - Do not forget that objects are passed by link. It can be somehow useful tho
- - The library is for working with async code. You always have to call output function inside your links.
--- taj.js
+++ taj.js
@@ -1,105 +1,211 @@
-var Taj = function (initVal, links) {
-  this.__queue = [];
-  this.__links = {};
-  this.__error_handler = function () {
+
+
+(function(global, varname){
+
+  var Lib = function () {
+    if(!this instanceof Lib) return new Lib();
+    this.__state = {
+      queue: [],
+      callbacks: {}
+    }
   };
-  if (initVal) this.set(initVal);
-  if (links) this.link(links);
-};
 
-var TajErr = function(type, exception, text, data){
-  this.type = type;this.exception = exception;this.text = text;this.data = data;
-}
+  Lib[varname] = Lib;
 
-Taj.prototype = {
-  __queue: null, __links: null, __error_handler: null,
-  __nextStep: function () {
-    var that = this;
-    if (this.__queue.length) {
-      var call_info = this.__queue.shift();
-
-      var cb;
-
-      if(call_info.type === 'link') {
-        cb = this.__links[call_info.name];
-      } else if(call_info.type === 'anonymous'){
-        cb = call_info.callback;
-      }
-
-      var outputFn = function (output) {
-        if(typeof output !== 'undefined') that.output = output;
-        that.__nextStep();
-      };
-      outputFn.pass = function(){
-        that.__nextStep();
-      };
-      outputFn.error = function (err) {
-        that.__error_handler(err);
-      };
-      outputFn.chain = this;
-
-      try {
-        cb.apply(outputFn, [this.output, outputFn].concat([].slice.call(call_info.args)));
-      } catch (e) {
-        this.__error_handler(new TajErr('callback_error', e, 'Error in ' + (call_info.name === undefined?'anonymous link':call_info.name), call_info));
-      }
-    } else {
-      this.atTheEnd(this.output);
-    }
-    return this;
-  },
-  set: function (input) {
-    this.output = input;
-    return this;
-  },
-  go: function (callback, value) {
-    if (typeof callback === 'function') {
-      this.onReady(callback);
-    }
-    if(value !== undefined) this.set(value);
-    return this.__nextStep();
-  },
-  onReady: function (callback) {
-    this.atTheEnd = callback;
-    return this;
-  },
-  onError: function (callback) {
-    this.__error_handler = callback;
-    return this;
-  },
-  link: function (name, callback) {
-    if (typeof name === 'object') {
-      for (var ln in name) {
-        if (name.hasOwnProperty(ln)) {
-          this.link(ln, name[ln]);
-        }
-      }
-    } else {
-      if (Object.keys(Taj.prototype).indexOf(name) !== -1) {
-        this.__error_handler(new TajErr('wrong_link_name',undefined, name+' is not allowed as a name of link.', name));
-      } else {
-        var that = this;
-        if (typeof callback === 'function') {
-          this.__links[name] = callback;
-          this[name] = function () {
-            that.__queue.push({type:'link', name: name, args: arguments});
-            return that;
-          };
-        } else {
-          this.__error_handler(new TajErr('link_not_a_function',undefined,'Not a function', callback));
-        }
-      }
-    }
-    return this;
-  },
-  next: function (callback) {
-    this.__queue.push({type:'anonymous', callback:callback, args:[]});
-    return this;
+  var isValidCallbackName = function(name){
+    return !Lib.prototype.hasOwnProperty(name);
   }
-};
 
-if(typeof module !== 'undefined' && module.exports) {
-  module.exports = Taj;
-} else {
-  this.Taj = Taj;
+  var addQueueStep = function(object, callback, arguments){
+    var queueObject = {
+      callback: callback,
+      arguments: arguments
+    };
+    object.__state.queue.push(queueObject);
+  }
+
+  var registerCallback = function(object, name, callback){
+    if(isValidCallbackName(name)){
+      object[name]=function(){
+        addQueueStep(object,callback,arguments);
+        return this;
+      }
+    }
+  };
+
+  Lib.prototype.__state = {queue:[],callbacks:{}};
+
+  Lib.prototype.registerCallback = function(name, callback){
+    if(typeof callback === 'function'){
+        registerCallback(this, name, callback);
+    }
+    return this;
+  };
+
+  Lib.prototype.resetQueue = function(){
+    this.__state.queue.queue.length = 0;
+  };
+
+  Lib.prototype.registerCallbacks = function(callbacks){
+    for (var callbackName in callbacks) {
+      if (callbacks.hasOwnProperty(callbackName)) {
+        this.registerCallback(callbackName, callbacks[callbackName]);
+      }
+    }
+    return this;
+  };
+
+  Lib.prototype.run = function(thisArgument, finalCallback){
+    var thisArg = thisArgument;
+    var instance = this;
+    var queueCounter = 0;
+    var nextStep = function(){
+      if(queueCounter<instance.__state.queue.length){
+        var queueStep = instance.__state.queue[queueCounter++];
+
+        var pass = function(value){
+          thisArg = value;
+          nextStep();
+        };
+        pass.error = function(error){
+          // Error handling
+        };
+
+        queueStep.callback.apply(thisArg, [].concat.apply([pass], queueStep.arguments));
+      } else {
+        if(typeof finalCallback === 'function') {
+          finalCallback.apply(thisArg, []);
+        }
+      }
+    };
+    nextStep();
+    return this;
+  };
+
+  Lib.prototype.then = function(callback,args){
+    addQueueStep(this,callback,args||[]);
+    return this;
+  };
+
+  Lib.prototype.getFunction = function(){
+    var chain = this;
+    return function (){
+      chain.run.apply(chain, arguments);
+    }
+  };
+
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Lib;
+  } else {
+    global[varname] = Lib;
+  }
+
+})(this, 'Taj');
+
+
+var da = function () {
+  var Taj = function (initVal, links) {
+    this.__queue = [];
+    this.__links = {};
+    this.__error_handler = function () {
+    };
+    if (initVal) this.set(initVal);
+    if (links) this.link(links);
+  };
+
+  var TajErr = function (type, exception, text, data) {
+    this.type = type;
+    this.exception = exception;
+    this.text = text;
+    this.data = data;
+  }
+
+  Taj.prototype = {
+    __queue: null, __links: null, __error_handler: null,
+    __nextStep: function () {
+      var that = this;
+      if (this.__queue.length) {
+        var call_info = this.__queue.shift();
+
+        var cb;
+
+        if (call_info.type === 'link') {
+          cb = this.__links[call_info.name];
+        } else if (call_info.type === 'anonymous') {
+          cb = call_info.callback;
+        }
+
+        var outputFn = function (output) {
+          if (typeof output !== 'undefined') that.output = output;
+          that.__nextStep();
+        };
+        outputFn.pass = function () {
+          that.__nextStep();
+        };
+        outputFn.error = function (err) {
+          that.__error_handler(err);
+        };
+        outputFn.chain = this;
+
+        try {
+          cb.apply(outputFn, [this.output, outputFn].concat([].slice.call(call_info.args)));
+        } catch (e) {
+          this.__error_handler(new TajErr('callback_error', e, 'Error in ' + (call_info.name === undefined ? 'anonymous link' : call_info.name), call_info));
+        }
+      } else {
+        this.atTheEnd(this.output);
+      }
+      return this;
+    },
+    set: function (input) {
+      this.output = input;
+      return this;
+    },
+
+    go: function (callback, value) {
+      if (typeof callback === 'function') {
+        this.onReady(callback);
+      }
+      if (value !== undefined) this.set(value);
+      return this.__nextStep();
+    },
+    onReady: function (callback) {
+      this.atTheEnd = callback;
+      return this;
+    },
+    onError: function (callback) {
+      this.__error_handler = callback;
+      return this;
+    },
+    link: function (name, callback) {
+      if (typeof name === 'object') {
+        for (var ln in name) {
+          if (name.hasOwnProperty(ln)) {
+            this.link(ln, name[ln]);
+          }
+        }
+      } else {
+        if (Object.keys(Taj.prototype).indexOf(name) !== -1) {
+          this.__error_handler(new TajErr('wrong_link_name', undefined, name + ' is not allowed as a name of link.', name));
+        } else {
+          var that = this;
+          if (typeof callback === 'function') {
+            this.__links[name] = callback;
+            this[name] = function () {
+              that.__queue.push({type: 'link', name: name, args: arguments});
+              return that;
+            };
+          } else {
+            this.__error_handler(new TajErr('link_not_a_function', undefined, 'Not a function', callback));
+          }
+        }
+      }
+      return this;
+    },
+    next: function (callback) {
+      this.__queue.push({type: 'anonymous', callback: callback, args: []});
+      return this;
+    }
+  };
 }
